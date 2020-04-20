b16250dd8f914b35f3b17dd2b77547c32123d99a
Andriy Lazorko <andriy_lazorko@ukr.net>
2020-04-20 19:03:19 +0300

    04201903

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(" GitConfig");
-        for (int i = 0; i < 3; i++)
-            System.out.println(i + " GitConfig ");
-    }
-}
