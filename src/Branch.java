750989f3d2d186c6e771d4665152ec8bd45a92fb
“Andriy <andriy_lazorko@ukr.net>
2020-05-03 14:05:54 +0300

    112. 05031405

--- src/Branch.java
+++ src/Branch.java
@@ -8,14 +8,35 @@ public class Branch {
         this.name = name;
         this.customers = new ArrayList<Customer>();
     }
+
     public String getName() {
         return name;
     }
-    public boolean newCustomer(String customerName, double initialAmount){
-        if(findCustomer(customerName)==null){
-            this.customers.add(new Customer(customerName,initialAmount));
+
+    public boolean newCustomer(String customerName, double initialAmount) {
+        if (findCustomer(customerName) == null) {
+            this.customers.add(new Customer(customerName, initialAmount));
             return true;
         }
+        return false;
+    }
 
+    public boolean addCustomerTransaction(String customerName, double amount) {
+        Customer existingCustomer = findCustomer(customerName);
+        if (existingCustomer != null) {
+            existingCustomer.addTransaction(amount);
+            return true;
+        }
+        return false;
+    }
+
+    private Customer findCustomer(String customerName) {
+        for (int i = 0; i < this.customers.size();i++ ){
+            Customer checkedCustomer = this.customers.get(i);
+            if(checkedCustomer.getName().equals(customerName)){
+                return checkedCustomer;
+            }
+        }
+        return null;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] a = new int[3];
+        a[2] = 9098;
+        a[0] = 1;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(a[i]);
+        }
     }
 }
