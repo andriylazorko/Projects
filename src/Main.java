8d14898612bc1b91eaa2a285cefb4b65425fe745
“Andriy <andriy_lazorko@ukr.net>
2020-05-04 17:05:25 +0300

    113. 05041704 Ending

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        int[] a = new int[3];
-        a[2] = 9098;
-        a[0] = 1;
-        for (int i = 0; i < 3; i++) {
-            System.out.println(a[i]);
+        System.out.println(" Exercise 112-114 ");
+        System.out.println(" Autoboxing and Unboxing ");
+
+        Bank bank = new Bank("National Australia Bank");
+
+        if(!bank.addBranch("Adelaide")){
+            System.out.println("Adelaide branch created");
         }
+
+        bank.addBranch("Adelaide");
+
+        bank.addCustomer("Adelaide","Tim",50.02);
+        bank.addCustomer("Adelaide","Mike",175.34);
+        bank.addCustomer("Adelaide","Percy",220.12);
+
+        bank.addBranch("Sydney");
+        bank.addCustomer("Sydney","Bob", 150.54);
+
+        bank.addCustomerTransaction("Adelaide","Tim",44.22);
+        bank.addCustomerTransaction("Adelaide","Tim",17.34);
+        bank.addCustomerTransaction("Adelaide","Mike",2120.19);
+
+        bank.listCustomers("Adelaide", true);
+        bank.listCustomers("Sydney",true);
+
+        bank.addBranch("Melbourne");
+        if(!bank.addCustomer("Melbourne","Brian",5.53)){
+            System.out.println("Error Melbourne branch does not exist");
+        }
+
+        if(!bank.addBranch("Adelaide")){
+            System.out.println("Adelaide branch already exists");
+        }
+
     }
 }
