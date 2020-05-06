55e41714d9228acf29453ab9c6c095b71293ad3e
“Andriy <andriy_lazorko@ukr.net>
2020-05-06 07:14:44 +0300

    myVersion 06050714

--- src/Bank.java
+++ src/Bank.java
@@ -2,11 +2,37 @@ import java.util.ArrayList;
 
 public class Bank {
     private String bankName;
-    private ArrayList<Branch>bankBranch;
+    private ArrayList<Branch> bankBranch;
 
     public Bank(String bankName) {
         this.bankName = bankName;
         this.bankBranch = new ArrayList<Branch>();
+    }
 
+    public boolean addCustomer(String branchName,
+                               String customerName, double transaction){
+        Branch searchBranch=search(branchName);
+        if(searchBranch!=null){
+            return searchBranch.addNewCustomer(customerName,transaction);
+        }
+        return false;
+    }
+
+    public boolean addBranch(String branchName){
+        if(search(branchName)==null){
+            this.bankBranch.add(new Branch(branchName));
+            return true;
+        }
+        return false;
+    }
+
+    private Branch search(String name) {
+        for (int i = 0; i < bankBranch.size(); i++) {
+            Branch searchBranch=bankBranch.get(i);
+            if(searchBranch.getBranchName().equals(name)){
+                return searchBranch;
+            }
+        }
+        return null;
     }
 }
