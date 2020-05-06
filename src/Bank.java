9f48dccdb91a96e8aa4b783a23103d744bd1ed53
“Andriy <andriy_lazorko@ukr.net>
2020-05-06 16:40:17 +0300

    myVersion 06051640

--- src/Bank.java
+++ src/Bank.java
@@ -9,17 +9,59 @@ public class Bank {
         this.bankBranch = new ArrayList<Branch>();
     }
 
-    public boolean addCustomer(String branchName,
-                               String customerName, double transaction){
-        Branch searchBranch=search(branchName);
-        if(searchBranch!=null){
-            return searchBranch.addNewCustomer(customerName,transaction);
+    public boolean listCustomer(String branchName, boolean showTransaction) {
+        Branch branch = search(branchName);
+        if (branch != null) {
+            System.out.println(bankName + "`s branch name is " + branch.getBranchName() +
+                    ", number of branch is " +
+                    bankBranch.indexOf(branch) + 1);
+            ArrayList<Customer> branchCustomer = branch.getBranchCustomer();
+            System.out.println(" List of customers:          Transaction:");
+            for (int i = 0; i < branchCustomer.size(); i++) {
+                System.out.print("  "+(i + 1) + ". ");
+                System.out.print(branchCustomer.get(i).getCustomerName());
+
+                if (showTransaction) {
+                    ArrayList<Double> transactions = branchCustomer.get(i).getTransactions();
+                    for (int j = 0; j < transactions.size(); j++) {
+                        if (j == 0) {
+                            System.out.println(":");
+                        }
+                        System.out.print("                              " + (j + 1) + ". " +
+                                transactions.get(j));
+                        System.out.println();
+                    }
+                } else {
+                    System.out.println();
+                }
+            }
+            return true;
+        } else {
+            System.out.println(" Branch " + branchName + " of " + bankName + " was not found.");
+            return false;
+        }
+    }
+
+    public boolean addTransaction(String branchName, String customerName, double transaction) {
+        Branch searchBranch = search(branchName);
+        if (searchBranch != null) {
+
+            return searchBranch.addNewTransaction(customerName, transaction);
         }
         return false;
     }
 
-    public boolean addBranch(String branchName){
-        if(search(branchName)==null){
+    public boolean addCustomer(String branchName,
+                               String customerName, double transaction) {
+        Branch searchBranch = search(branchName);
+        if (searchBranch != null) {
+            return searchBranch.addNewCustomer(customerName, transaction);
+        }
+        return false;
+    }
+
+    public boolean addBranch(String branchName) {
+        if (search(branchName) == null) {
             this.bankBranch.add(new Branch(branchName));
             return true;
         }
@@ -28,8 +70,8 @@ public class Bank {
 
     private Branch search(String name) {
         for (int i = 0; i < bankBranch.size(); i++) {
-            Branch searchBranch=bankBranch.get(i);
-            if(searchBranch.getBranchName().equals(name)){
+            Branch searchBranch = bankBranch.get(i);
+            if (searchBranch.getBranchName().equals(name)) {
                 return searchBranch;
             }
         }
