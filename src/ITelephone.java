de4ae378c8f2e63b2d9960853b5409fa8815cedb
“Andriy <andriy_lazorko@ukr.net>
2020-06-15 17:30:54 +0300

    16061730

--- src/ITelephone.java
+++
@@ -1,8 +0,0 @@
-public interface ITelephone {
-    public void powerOn();
-    public void dial(int phoneNumber);
-    public void answer();
-    public boolean callPhone(int phoneNumber);
-    public boolean isRinging();
-
-}
