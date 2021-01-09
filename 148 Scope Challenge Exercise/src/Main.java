08243f8457e6e9847039b6e56e03223756e7b621
“Andriy <andriy_lazorko@ukr.net>
2021-01-09 08:33:41 +0200

    0901210833

---
+++ 148 Scope Challenge Exercise/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("already complete");
+
+        System.out.println("Please enter a number: ");
+        X x = new X(new Scanner(System.in));
+        x.x();
+    }
+}
