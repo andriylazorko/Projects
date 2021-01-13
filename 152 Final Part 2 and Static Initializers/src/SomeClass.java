606f02a35cd6d8b5936976204ef58bd448cc5266
“Andriy <andriy_lazorko@ukr.net>
2021-01-13 15:51:56 +0200

    1301211551

---
+++ 152 Final Part 2 and Static Initializers/src/SomeClass.java
@@ -0,0 +1,16 @@
+public class SomeClass {
+    private static int classCounter=0;
+    public final int instanceNumber;
+    private final String name;
+
+    public SomeClass(String name){
+        classCounter++;
+        instanceNumber=classCounter;
+        this.name=name;
+        System.out.println(name+" created, instance is "+instanceNumber);
+    }
+
+    public int getInstanceNumber() {
+        return instanceNumber;
+    }
+}
