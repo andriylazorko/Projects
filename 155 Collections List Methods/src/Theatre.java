280b51e453743686a45e2fb8d4e05490e555e7d5
“Andriy <andriy_lazorko@ukr.net>
2021-01-20 17:34:21 +0200

    2001211734

--- 155 Collections List Methods/src/Theatre.java
+++ 155 Collections List Methods/src/Theatre.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Theatre {
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
+    public List<Seat> seats = new ArrayList<>();
 
     public Theatre(String theatreName, int numRows, int seatsPerRows) {
         this.theatreName = theatreName;
@@ -73,7 +73,7 @@ public class Theatre {
         }
     }
 
-    private class Seat implements Comparable<Seat> {
+    public class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
 
@@ -86,7 +86,7 @@ public class Theatre {
             return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
         }
 
-        private boolean reserve() {
+        public boolean reserve() {
             if (!this.reserved == true) {
                 this.reserved = true;
                 System.out.println("Reservation of seat " + seatNumber + " reserved");
