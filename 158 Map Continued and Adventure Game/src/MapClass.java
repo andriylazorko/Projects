381af34bc77b551a4d129bcc7f6b9d48d6d5c1f2
“Andriy <andriy_lazorko@ukr.net>
2021-01-29 08:45:07 +0200

    2901210845

---
+++ 158 Map Continued and Adventure Game/src/MapClass.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapClass {
+    public static void main(String[] args) {
+        Map<String, String> language = new HashMap<>();
+        if (language.containsKey("Java")) {
+            System.out.println("Java is already exist");
+        } else {
+            language.put("Java", "a compiled high level, object-oriented, platform independent language");
+            System.out.println("Java added successfully");
+        }
+
+        language.put("Phython", "an interpreted,object-oriented, high level programing language with dynamic semantics");
+        language.put("Algol", "an algorithmic language");
+        System.out.println(language.put("BASIC", "Beginners All Purposes Symbolic Instructions Code"));
+        System.out.println(language.put("Lisp", "Therein lies madness"));
+
+        if (language.containsKey("Java")) {
+            System.out.println("Java is already in the map");
+        } else {
+            System.out.println(language.put("Java", "this course is about Java"));
+
+        }
+        System.out.println("===================================");
+
+        for(String key:language.keySet()){
+            System.out.println(key+":"+language.get(key));
+        }
+    }
+}
