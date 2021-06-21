2417e8728e532ebb864e688047da2989eba23106
“Andriy <andriy_lazorko@ukr.net>
2021-06-21 07:28:37 +0300

    2106210728

--- secondLesson/src/Main.java
+++ secondLesson/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello second lesson");
         for (int i = 0; i < 4; i++) {
-            System.out.print(i + "");
+            System.out.print(i + " ");
         }
     }
 }
