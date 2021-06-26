2ceabad23a39e673db856adc9ad133530303b723
--add <andriy.valentinovi4@gmail.com>
2021-06-26 16:35:11 +0300

    2106261634

--- secondLesson/src/Main.java
+++ secondLesson/src/Main.java
@@ -1,8 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello second lesson");
-        for (int i = 0; i < 4; i++) {
-            System.out.print(i + " ");
+        int numOfPrime = 20;//Integer.parseInt(args[0]);
+        System.out.print(primeNumbers(numOfPrime));
+    }
+
+    public static String primeNumbers(int n) {
+        int i = 0;
+        int num = 0;
+        boolean achieve = true;
+        int primeCount = 0;
+        String primeNumbers = "";
+        while (achieve) {
+
+            int counter = 0;
+            for (num = i; num >= 1; num--) {
+                if (i % num == 0) {
+                    counter = counter + 1;
+                }
+            }
+            if (counter == 2) {
+                primeNumbers = primeNumbers + i + ", ";
+                primeCount++;
+            }
+            i++;
+            if(primeCount==n){
+                achieve=false;
+            }
         }
+        return primeNumbers;
     }
 }
